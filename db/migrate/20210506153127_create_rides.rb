ca671911e19f277043ae95a6716a055a96366b3c
Martin <Martin@Martin>
2023-06-29 13:50:51 +0300

    Completed assignment

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
 
+      t.references :taxi, null: false, foreign_key: true
+      t.references :passenger, null: false, foreign_key: true
       t.timestamps
     end
   end
